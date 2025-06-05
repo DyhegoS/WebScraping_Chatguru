0a3cb67748a1145c5f896d420735e7b885e382cc
DyhegoS <dyhegodasilva@gmail.com>
2025-06-05 14:44:58 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,118 @@
+# type: ignore
+# Selenium - Automatizando tarefas no navegador
+import pandas as pd
+from pathlib import Path
+from time import sleep
+import getpass
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import WebDriverException
+import traceback
+
+# Chrome Options
+# https://peter.sh/experiments/chromium-command-line-switches/
+# Doc Selenium
+# https://selenium-python.readthedocs.io/locating-elements.html
+
+
+ROOT_FOLDER = Path(__file__).parent
+CHROME_DRIVER_PATH = ROOT_FOLDER / 'driver' / 'chromedriver.exe'
+
+
+def make_chrome_browser(*options: str) -> webdriver.Chrome:
+    chrome_options = webdriver.ChromeOptions()
+
+    # chrome_options.add_argument('--headless')
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(
+        executable_path=str(CHROME_DRIVER_PATH),
+    )
+
+    browser = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options
+    )
+
+    return browser
+
+
+if __name__ == '__main__':
+    TIME_TO_WAIT = 500
+
+    options = '--disable-gpu',
+    browser = make_chrome_browser(*options)
+
+    browser.get('https://s17.chatguru.app/')
+
+    login = browser.find_element(By.ID, 'email')
+    password = browser.find_element(By.ID, 'password')
+    login.send_keys('suporteti@7oliveiras.com.br')
+    my_password = getpass.getpass()
+    password.send_keys(my_password)
+    browser.find_element(By.CSS_SELECTOR, 'button.FormButton').click()
+try:
+    access_chats = WebDriverWait(browser, 200).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[href="/users"]'))
+    )
+
+    access_chats.click()
+except:
+    print('link não encontrado')
+try:
+    # Espera o <tbody> carregar
+    tbody = WebDriverWait(browser, 300).until(
+        EC.presence_of_element_located((By.TAG_NAME, "tbody"))
+    )
+
+    linhas = tbody.find_elements(By.TAG_NAME, "tr")
+
+    dados = []
+
+    for linha in linhas:
+        colunas = linha.find_elements(By.TAG_NAME, "td")
+    
+    if len(colunas) >= 4:
+        # NOME: dentro de <a>
+        nome = colunas[1].find_element(By.TAG_NAME, "a").text
+        
+        # E-MAIL: dentro de <span> no mesmo <td>
+        email = colunas[1].find_element(By.TAG_NAME, "span").text
+        
+        # STATUS LOGIN: dentro de <span>
+        status_login = colunas[5].find_element(By.TAG_NAME, "span").text
+        
+        # ÚLTIMO ACESSO: texto puro
+        ultimo_acesso = colunas[6].text.strip()
+        
+        # VISTO ÚLTIMA VEZ: texto puro
+        visto_ultima_vez = colunas[7].text.strip()
+        
+    dados.append({
+        'Nome': nome,
+        'E-mail': email,
+        'Status Login': status_login,
+        'Último Acesso': ultimo_acesso,
+        'Visto Última Vez': visto_ultima_vez
+    })
+
+    for itens in dados:
+        df = pd.DataFrame(dados)
+        df.to_excel('usuarios.xlsx', index=False)
+
+    print("Exportado com sucesso para 'usuarios.xlsx'")
+except WebDriverException as e:
+    print("Erro ao coletar table e gerar .xlsx")
+    print(e)
+
+    print("Stacktrace")
+    traceback.print_exc()
+finally:
+    browser.quit()
+    sleep(TIME_TO_WAIT)
